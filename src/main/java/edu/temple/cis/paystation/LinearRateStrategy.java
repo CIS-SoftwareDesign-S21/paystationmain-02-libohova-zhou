7e5b9804250fb18f0f2229f2bdcec49e16098a46
Mlibohova <tuh34713@temple.edu>
2021-02-14 12:11:21 -0500

    RateStrategy classes update
    Progressive and Linear done

--- src/main/java/edu/temple/cis/paystation/LinearRateStrategy.java
+++ src/main/java/edu/temple/cis/paystation/LinearRateStrategy.java
@@ -1,4 +1,13 @@
 package edu.temple.cis.paystation;
 
+/**
+ *  Linear Rate Strategy
+ *  Time = (amount * 2) / 5
+ */
+
+
 public class LinearRateStrategy implements RateStrategy {
+    public int calculateTime(int interestedSoFar){
+        return interestedSoFar / 5 * 2;
+    }
 }
