16bfeb01c5d9b15a1d92ed4ec7aa6ce8cdd4004d
Mlibohova <tuh34713@temple.edu>
2021-02-15 18:56:07 -0500

    Update and Added for Weekend Strategy
    Updated AlternatingStrategy
    Added classes for weekend

---
+++ src/main/java/edu/temple/cis/paystation/WeekendStrategyPick.java
@@ -0,0 +1,13 @@
+package edu.temple.cis.paystation;
+
+//class  for theWeekend
+
+public class WeekendStrategyPick {
+    private boolean theWeekend;
+    public WeekendStrategyPick(boolean theWeekend){
+        this.theWeekend = theWeekend;
+    }
+    public boolean theWeekend(){
+        return theWeekend;
+    }
+}
